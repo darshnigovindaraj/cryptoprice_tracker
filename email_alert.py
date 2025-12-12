cf4afba9cac3ecc025d77d968ae06972b1b142ec
Darshni G <darshnigovind2005@gmail.com>
2025-12-12 20:36:46 +0530

    Update email_alert.py

--- email_alert.py
+++ email_alert.py
@@ -1,8 +1,8 @@
 import yagmail
 
-SENDER_EMAIL = "xxx enter your email"
-APP_PASSWORD = "yyy"
-RECEIVER_EMAIL = "xxx "
+SENDER_EMAIL = "enter your email"
+APP_PASSWORD = "enter your password"
+RECEIVER_EMAIL = "enter an another email to send the alert"
 
 def send_email(subject, body):
     yag = yagmail.SMTP(SENDER_EMAIL, APP_PASSWORD)
